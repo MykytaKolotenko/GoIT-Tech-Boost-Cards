68c60dc62b27ea3149ebc8233a96af5a092ca2ec
Никита Колотенко <tofeee63@gmail.com>
2023-04-18 20:42:56 +0300

    end

--- src/Redux/userApi.tsx
+++ src/Redux/userApi.tsx
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import IUser from '../interface/IUser';
 
-
-
 export interface IUserPutQuery {
     id: string;
     followers: number;
